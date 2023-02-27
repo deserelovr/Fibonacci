b3120fe2d3d586c50384449912a9287935cead8e
jasmine.bumbray@my.strayer.edu <jasmine.bumbray@my.strayer.edu>
2023-02-26 16:10:43 -0800

    test with range of 4

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,1 +1,19 @@
+#Jasmine Bumbray
+#CIS 261
+#Recursive Function
 
+def fib(b):
+    if b == 0:
+        return 0 #only works with 0 tried 1,2,and 3
+    elif b == 1:
+        return 1
+    else:
+        return fib(b - 1) + fib(b - 2) #will add the number infront to get the next number
+
+def main():
+    for i in range(16):
+        print(fib(i), end=', ')
+    print('. . .')
+
+if __name__ == "__main__":
+    main()
